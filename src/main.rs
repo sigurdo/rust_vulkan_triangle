608f6c7ae5ef769c5bedaeafe3eb4df71ec4b0ad
Sigurd Tullander <legotull.99@gmail.com>
2022-07-26 23:12:05 +0200

    Create instance, event loop and surface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use vulkano::{
+    device::{DeviceExtensions},
+    instance::{Instance, InstanceCreateInfo},
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, vulkano!");
+
+    let required_extensions = vulkano_win::required_extensions();
+
+    let instance = Instance::new(InstanceCreateInfo {
+        enabled_extensions: required_extensions,
+        enumerate_portability: true,
+        ..Default::default()
+    })
+    .unwrap();
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
 }
